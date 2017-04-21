13279faa1fc5903982e3834fe565b3b43a3f0f9c
Erick Brito <ebrito@ebrito.local>
2017-04-21 13:14:28 -0400

    adding /api/hola endpoint

---
+++ src/main/java/com/example/HolaRestController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by ebrito on 2017-04-21.
+ */
+@RestController
+@RequestMapping("/api")
+public class HolaRestController {
+    @RequestMapping(method = RequestMethod.GET, value = "/hola", produces = "text/plain")
+    public String hola() throws UnknownHostException {
+        String hostname = null;
+        try {
+            hostname = InetAddress.getLocalHost().getHostAddress();
+
+        }catch (UnknownHostException e){
+            hostname = "unknown";
+        }
+        return "Hola Spring Boot de " + hostname;
+    }
+}
